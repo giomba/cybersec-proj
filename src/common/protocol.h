08ca85f52bc2426ca7e06e65854f8532e85e8ea8
francesco.barbarulo <barbarulofrancesco@gmail.com>
2019-04-18 11:33:52 +0200

    Added header file for protocol

---
+++ src/common/protocol.h
@@ -0,0 +1,10 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define OK                       200
+#define SERVER_ERROR             500
+#define SYNTAX_ERROR             501
+#define COMMAND_NOT_IMPLEMENTED  502
+#define BAD_SEQUENCE_OF_COMMANDS 503
+
+#endif
